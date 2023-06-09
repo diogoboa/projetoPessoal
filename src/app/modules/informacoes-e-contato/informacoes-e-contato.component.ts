721ff965fb036d877f1e37d8883d3a621f8522dd
diogo <diogo.boaventura.ufms@gmail.com>
2023-06-09 15:13:01 -0300

    pages

---
+++ src/app/modules/informacoes-e-contato/informacoes-e-contato.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-informacoes-e-contato',
+  templateUrl: './informacoes-e-contato.component.html',
+  styleUrls: ['./informacoes-e-contato.component.css']
+})
+export class InformacoesEContatoComponent {
+
+}
