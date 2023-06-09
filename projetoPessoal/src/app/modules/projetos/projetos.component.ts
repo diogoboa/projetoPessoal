ca992f68ffefcc8db072bf83a3b2e83118a8ea6a
diogo <diogo.boaventura.ufms@gmail.com>
2023-06-09 12:59:03 -0300

    aprendendo angular

--- projetoPessoal/src/app/modules/projetos/projetos.component.ts
+++ projetoPessoal/src/app/modules/projetos/projetos.component.ts
@@ -8,121 +8,185 @@ import { Lightbox } from 'ngx-lightbox';
 })
 export class ProjetosComponent implements OnInit {
 
-  albums: any[] = []; // Initialize the albums array
+  album_docas_grupo_penta: any[] = [];
+
+   albums_sispenta: any[] = [];
 
   constructor(private ngZone: NgZone, private lightbox: Lightbox) {}
 
   ngOnInit() {
     this.ngZone.runOutsideAngular(() => {
       // Create your album with images
-      this.albums = [
+      this.album_docas_grupo_penta = [
         {
           src: '/assets/images/projetos/docasGrupoPenta/01.png',
-          caption: 'Image 1',
-          thumb: 'https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-200.jpg'
+          caption: '',
+          thumb: ''
         },
         {
           src: '/assets/images/projetos/docasGrupoPenta/02.jpeg',
-          caption: 'Image 2',
-          thumb: 'https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-200.jpg'
+          caption: '',
+          thumb: ''
         },
         {
           src: '/assets/images/projetos/docasGrupoPenta/03.jpeg',
-          caption: 'Image 2',
-          thumb: 'https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-200.jpg'
+          caption: '',
+          thumb: ''
         },
         {
           src: '/assets/images/projetos/docasGrupoPenta/04.jpeg',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         },
         {
           src: '/assets/images/projetos/docasGrupoPenta/05.png',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         },
 
         {
           src: '/assets/images/projetos/docasGrupoPenta/06.jpeg',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         },
 
         {
           src: '/assets/images/projetos/docasGrupoPenta/07.png',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         },
 
         {
           src: '/assets/images/projetos/docasGrupoPenta/08.jpeg',
           caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          thumb: ''
         },
 
         {
           src: '/assets/images/projetos/docasGrupoPenta/09.png',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         },
 
         {
           src: '/assets/images/projetos/docasGrupoPenta/12.jpeg',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         },
 
         {
           src: '/assets/images/projetos/docasGrupoPenta/13.jpeg',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         },
 
         {
           src: '/assets/images/projetos/docasGrupoPenta/14.png',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         },
 
         {
           src: '/assets/images/projetos/docasGrupoPenta/15.jpeg',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         },
 
         {
           src: '/assets/images/projetos/docasGrupoPenta/16.jpeg',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         },
 
         {
           src: '/assets/images/projetos/docasGrupoPenta/17.jpeg',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         },
 
         {
           src: '/assets/images/projetos/docasGrupoPenta/18.jpeg',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         },
 
         {
           src: '/assets/images/projetos/docasGrupoPenta/19.jpeg',
-          caption: 'Image 2',
-          thumb: 'https://www.w3schools.com/howto/img_snow_wide.jpg'
+          caption: '',
+          thumb: ''
         }
 
 
 
       ];
+
+
+      this.albums_sispenta = [
+        {
+          src: '/assets/images/projetos/sisPenta/01.jpg',
+          caption: '',
+          thumb: ''
+        },
+        {
+          src: '/assets/images/projetos/sisPenta/02.jpg',
+          caption: '',
+          thumb: ''
+        },
+        {
+          src: '/assets/images/projetos/sisPenta/03.jpg',
+          caption: '',
+          thumb: ''
+        },
+        {
+          src: '/assets/images/projetos/sisPenta/04.jpg',
+          caption: '',
+          thumb: ''
+        },
+        {
+          src: '/assets/images/projetos/sisPenta/05.jpg',
+          caption: '',
+          thumb: ''
+        },
+
+        {
+          src: '/assets/images/projetos/sisPenta/07.jpg',
+          caption: '',
+          thumb: ''
+        },
+
+        {
+          src: '/assets/images/projetos/sisPenta/08.jpg',
+          caption: '',
+          thumb: ''
+        },
+
+        {
+          src: '/assets/images/projetos/sisPenta/09.jpg',
+          caption: '',
+          thumb: ''
+        },
+
+        {
+          src: '/assets/images/projetos/sisPenta/10.jpg',
+          caption: '',
+          thumb: ''
+        },
+
+        {
+          src: '/assets/images/projetos/sisPenta/11.jpg',
+          caption: '',
+          thumb: ''
+        },
+
+
+
+      ];
+
+
     });
   }
 
-  openLightbox(index: number): void {
-    this.lightbox.open(this.albums, index);
-  }
+
 
 
 }
